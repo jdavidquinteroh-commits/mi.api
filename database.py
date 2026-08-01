d6ea798c02296776df37edb5a994372dd78495d6
David <jdavidquinteroh@gmail.com>
2026-07-31 22:07:00 -0500

    fix DATABASE_URL con valor por defecto

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
-﻿from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+﻿import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
-from dotenv import load_dotenv
-import os
-
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./productos.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
